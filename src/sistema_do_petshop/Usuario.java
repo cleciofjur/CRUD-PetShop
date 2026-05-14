4544690ef97fc9cab90cf418a146153bf9e34586
Clecio Júnior <cleciofjur21@gmail.com>
2026-05-14 16:28:11 -0400

    Update Usuario.java

--- src/sistema_do_petshop/Usuario.java
+++ src/sistema_do_petshop/Usuario.java
@@ -5,18 +5,24 @@ public class Usuario {
 	private String senha;
 	private String cargo;
 	
+	// Construtor com parametro vazio
 	Usuario() {}
-	
+
+	// Construtor com a passagem de parâmetro
 	Usuario (String nome, String senha, String cargo) {
-		this.nome = nome;
+		this.nome = nome; // "this" faz referência ao atributo da classe
 		this.senha = senha;
 		this.cargo = cargo;
 	}
-	
+
+	// Getters e Setter são modificadores de acesso na linguagem, eles servem para modificar ou acessar os atributos privados de forma controlada e correta
+
+	// o Set faz com que valor seja lido
 	public void setNome (String nome) {
 		this.nome = nome;
 	}
-	
+
+	// o Get define ou atualiza o valor permitindo validações
 	public String getNome () {
 		return nome;
 	}
@@ -36,8 +42,11 @@ public class Usuario {
 	public String getCargo () {
 		return cargo;
 	}
-	
+
+	// "public boolean" é uma função que retorna um valor 'True' ou 'False' dependendo da condição
+	// nomeTentativa e senhaTentativa são as variavéis que o usuário digitou 
 	public boolean autenticar (String nomeTentaiva, String senhaTentativa) {
+		// 'this.nome.equals' ou 'this.senha.equals' faz uma verificação se os dados digitados pelo usuário são os mesmos da base de dados
 		return this.nome.equals(nomeTentaiva) && this.senha.equals(senhaTentativa);
 	}
 }
